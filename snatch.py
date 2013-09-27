b217ac9df833b8780d35b2c0196861548f02bb4f
thimos <thimos@localhost>
2013-09-26 23:54:15 -0700

    reorganize code for clarity, add tumblr support

--- snatch.py
+++ snatch.py
@@ -25,25 +25,12 @@ class RSnatcher(object):
             '.*(' +
             '|'.join([re.escape(d) for d in self.video_domains]) +
             ').*')
+        self.tumblr_video_regex = re.compile(
+            r'source src=\\x22(.*video_file.*)\\x22 type=\\x22video/mp4\\x22'
+            )
 
     def download(self, url, title=None, length=None, user=None):
-        filename = url.split('/')[-1]
-        filename = filename.split('?')[0]
-        if title:
-            filename = title + '.' + filename.split('.')[-1]
-        filename = os.path.basename(filename)
-
-        if self.subdirs and user:
-            dirname = os.path.basename(user)
-            if not os.path.exists(dirname):
-                os.mkdir(dirname)
-
-            filename = os.path.join(dirname, filename)
-
-        if os.path.exists(filename):
-            print("{}: file already exists".format(filename))
-            return
-
+        # we do this to get the final URL
         r = requests.get(
             url,
             stream=True,
@@ -51,6 +38,28 @@ class RSnatcher(object):
         downloaded = 0.0
         chunk_size = 1024
 
+        # work out the filename
+        filename = r.url.split('/')[-1]
+        filename = filename.split('?')[0]
+        filename = filename.split('#')[0]
+        if title:
+            filename = title + '.' + filename.split('.')[-1]
+        filename = os.path.basename(filename)
+
+        # split into separate subdirectories when requested
+        if self.subdirs and user:
+            dirname = os.path.basename(user)
+            if not os.path.exists(dirname):
+                os.mkdir(dirname)
+
+            filename = os.path.join(dirname, filename)
+
+        # check that file does not already exist
+        if os.path.exists(filename):
+            print("{}: file already exists".format(filename))
+            return
+
+        # determine file length if it was not given
         if not length and 'content-length' in r.headers:
             length = int(r.headers['content-length'])
 
@@ -58,6 +67,7 @@ class RSnatcher(object):
             pbar = progressbar.ProgressBar(maxval=length)
             pbar.start()
 
+        # download the file in chunks to conserve memory and track pprogress
         with open(filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=chunk_size):
                 if chunk:
@@ -89,11 +99,12 @@ class RSnatcher(object):
             else:
                 author = None
 
-
+            # check if this post is an image
             if self.image_regex.match(url):
                 print((subreddit, title, author, url))
                 self.download(url, user=author)
 
+            # check if this post is a video
             if self.video_regex.match(url):
                 print((subreddit, title, author, url))
 
@@ -104,6 +115,13 @@ class RSnatcher(object):
                     title=media['page_title'],
                     length=int(media['link'][0]['length_bytes']),
                     user=author)
+
+            # check if this post is a tumblr link
+            if 'tumblr.com/post' in url:
+                r = requests.get(url, headers={'User-Agent': self.user_agent})
+                m = self.tumblr_video_regex.search(r.text)
+                if m:
+                    self.download(m.group(1), user=author)
 
 
 if __name__ == "__main__":
@@ -117,7 +135,6 @@ if __name__ == "__main__":
     parser.add_argument(
         '-s',
         '--subdirs',
-        type=bool,
         default=False,
         action='store_true',
         help="Create subdirectories for posts by each user.")
